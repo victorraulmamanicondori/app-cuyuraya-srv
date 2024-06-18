745966892a0a708b515256dccc840a1f71ec0bc8
Victor Raul Mamani Condori <victorr.mamanic@gmail.com>
2024-06-18 07:40:42 -0500

    Se agrega documentacion a nivel metodo

--- src/rutas/lecturaRutas.js
+++ src/rutas/lecturaRutas.js
@@ -4,6 +4,7 @@ import { verificarToken } from '../middlewares/autenticacionMiddleware.js';
 
 const router = express.Router();
 
+// La ruta '/' se asocia al middleware y metodo del controlador
 router.post('/', verificarToken, lecturaControlador.registrarLectura);
 
 export default router;
