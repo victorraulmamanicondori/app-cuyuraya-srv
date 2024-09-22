8d8c21f45406d2fe368a3725fae501481a0c8b4c
Victor Raul Mamani Condori <victorr.mamanic@gmail.com>
2024-09-22 13:06:39 -0500

    Se agrega scripts python para cargar ubigeo, comunidades

---
+++ cargar-ubigeo/cargaComNativa.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import requests
+import glob
+import os
+
+#base_url = "http://104.251.212.105:9000/api"
+base_url = "http://127.0.0.1:9000/api"
+
+headers = {
+    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiNDYzOTA1NTAiLCJpYXQiOjE3MjcwMjU5NzgsImV4cCI6MTcyNzA0Mzk3OH0.oJgnWs5P2Kr9TNE0Y4dSAhxQxd1Rw0cw4Xd4Q07yeYE",
+    "Content-Type": "application/json"
+}
+
+def doPOST(excelName):
+	df = pd.read_excel(excelName, sheet_name='comunidadNativa', engine='xlrd', skiprows=3)
+	df = df.fillna('')
+
+	for index, row in df.iterrows():
+		department = row['DEPARTAMENTO'].strip()
+
+		if department == 'Instituto Nacional de Estadística e Informática' \
+			or department == 'DEPARTAMENTO' or department == '/* La ubicación de la Comunidad Nativa se encuentra en evaluación cartográfica':
+		    continue
+
+		province = row['PROVINCIA'].strip()
+		district = row['DISTRITO'].strip()
+		familiaLinguistica = row['FAM. LINGUISTICA'].strip()
+		etnia = row['ETNIA'].strip()
+		comunidadNativa = row['COMUNIDAD NATIVA'].strip()
+		
+		dis_code = district[0:6]
+		dis_name = district[6:].strip()
+
+		cN_code = comunidadNativa[0:4]
+		cN_name = comunidadNativa[4:].strip()
+
+		payload = {
+			"codigo": f"{dis_code}{cN_code}",
+			"nombre": cN_name,
+			"familiaLinguistica": familiaLinguistica,
+			"etnia": etnia
+		}
+
+		response = requests.post(f"{base_url}/comunidades-nativas", headers=headers, json=payload)
+
+		if response.status_code == 200 or response.status_code == 201:
+		    #print("Registro exitoso:", response.text)
+		    pass
+		else:
+		    print("Error:", response.status_code, response.text)
+		    print(f"{department}-{province}-{dis_code}{dis_name}-{dis_code}{cN_code}{cN_name}")
+
+directorio="/home/miguel/Projects/eas/cargar-ubigeo"
+patron = os.path.join(directorio, "rptComunidadNativa_amazonas.xls")
+archivos = glob.glob(patron)
+
+for archivo in archivos:
+	doPOST(archivo)
+
---
+++ cargar-ubigeo/cargaUbigeo.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import requests
+
+# Load the Excel file
+df = pd.read_excel('rptUbigeo.xls', sheet_name='ubigeo', engine='xlrd')  # Use 'xlrd' for .xls files
+
+# Display the first few rows
+print(df.head())
+
+#base_url = "http://104.251.212.105:9000/api"
+base_url = "http://127.0.0.1:9000/api"
+
+headers = {
+    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiNDYzOTA1NTAiLCJpYXQiOjE3MjcwMTIwNzQsImV4cCI6MTcyNzAzMDA3NH0.7aQl16FnTHk9QUZV6QT9C_Z91rAAXKmUHE20De8590M",
+    "Content-Type": "application/json"
+}
+
+data = {}
+
+for index, row in df.iterrows():
+	department = row['DEPARTAMENTO'].strip()
+	province = row['PROVINCIA'].strip()
+	district = row['DISTRITO'].strip()
+
+	# Department
+	dep_code = department[0:2]
+	dep_name = department[2:].strip()
+
+	if len(dep_code) and dep_code not in data:
+		data[dep_code] = {
+			'code': dep_code,
+			'name': dep_name,
+			'provinces': {}
+		}
+
+		payload = {
+			"codigo": dep_code,
+			"nombre": dep_name
+		}
+
+		response = requests.post(f"{base_url}/departamentos", headers=headers, json=payload)
+
+		if response.status_code == 200 or response.status_code == 201:
+		    print("Registro exitoso:", response.text)
+		else:
+		    print("Error:", response.status_code, response.text)
+
+	# Province
+	pro_code = province[0:2]
+	pro_name = province[2:].strip()
+
+	if len(pro_code) > 0 and pro_code not in data[dep_code]['provinces']:
+		data[dep_code]['provinces'][pro_code] = {
+			'code': pro_code,
+			'name': pro_name,
+			'districts': []
+		}
+
+		payload = {
+			"codigo": f"{dep_code}{pro_code}",
+			"nombre": pro_name
+		}
+
+		response = requests.post(f"{base_url}/provincias", headers=headers, json=payload)
+
+		if response.status_code == 200 or response.status_code == 201:
+		    print("Registro exitoso:", response.text)
+		else:
+		    print("Error:", response.status_code, response.text)
+
+	# District
+	dis_code = district[0:2]
+	dis_name = district[2:].strip()
+
+	if len(dis_code) > 0:
+		data[dep_code]['provinces'][pro_code]['districts'].append({
+			'code': dis_code,
+			'name': dis_name
+		})
+
+		payload = {
+			"codigo": f"{dep_code}{pro_code}{dis_code}",
+			"nombre": dis_name
+		}
+
+		response = requests.post(f"{base_url}/distritos", headers=headers, json=payload)
+
+		if response.status_code == 200 or response.status_code == 201:
+		    print("Registro exitoso:", response.text)
+		else:
+		    print("Error:", response.status_code, response.text)
+
+	
