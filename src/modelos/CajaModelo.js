fab94b0e6525e331575870b9047a22e9c4c58422
Victor Raul Mamani Condori <victorr.mamanic@gmail.com>
2024-12-10 10:03:03 -0500

    Se agrega otro reporte de egresos o ingresos

--- src/modelos/CajaModelo.js
+++ src/modelos/CajaModelo.js
@@ -1,7 +1,7 @@
 class CajaModelo {
   constructor({ idCaja, numeroComprobante, idTipoMovimiento, 
                 fechaMovimiento, descripcion, monto, 
-                idPagoRecibo, estado, fecCreacion, fecActualizacion }) {
+                idPagoRecibo, estado, fecCreacion, fecActualizacion, concepto }) {
     this.idCaja = idCaja;
     this.numeroComprobante = numeroComprobante;
     this.idTipoMovimiento = idTipoMovimiento;
@@ -12,6 +12,16 @@ class CajaModelo {
     this.estado = estado;
     this.fecCreacion = fecCreacion;
     this.fecActualizacion = fecActualizacion;
+    this.concepto = concepto;
+  }
+
+  obtenerMes() {
+    const meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+    const [dia, mes, anio] = this.fechaMovimiento.split("/");
+    return meses[parseInt(mes, 10) - 1];
   }
 }
 
